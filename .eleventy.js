1acd058fa5b49d5193190af24ca1010d9e26c1bd
mensigo <menshikh.ig@gmail.com>
2026-03-17 00:49:36 +0300

    make new font local

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy("src/fonts");
 
     // Фильтр для читаемой даты
     eleventyConfig.addFilter("readableDate", (dateObj) => {
